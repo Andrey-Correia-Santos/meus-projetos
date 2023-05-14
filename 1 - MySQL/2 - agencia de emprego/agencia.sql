3cdb7769701455974876773e8aa633f883655a98
Andrey-Correia-Santos <123602749+Andrey-Correia-Santos@users.noreply.github.com>
2023-05-14 17:42:48 -0300

    Update agencia.sql
    - Alteração nas tabelas

--- 1 - MySQL/2 - agencia de emprego/agencia.sql
+++ 1 - MySQL/2 - agencia de emprego/agencia.sql
@@ -2,116 +2,153 @@ create database agencia;
 
 use agencia;
 
-
 CREATE TABLE cidade (
-    id smallint PRIMARY KEY auto_increment not null,
+    id smallint auto_increment not null PRIMARY KEY,
     nome varchar(80) not null,
     estado char(2) not null
 ) default charset utf8;
 
+CREATE TABLE Estado_civil (
+    id tinyint auto_increment not null PRIMARY KEY,
+    estado varchar(30) not null unique
+)default charset utf8;
 
-CREATE TABLE estado_civil (
-    id tinyint PRIMARY KEY auto_increment not null,
-    estado varchar(30) unique
-) default charset utf8;
-
-
-CREATE TABLE profissoes (
-    id tinyint PRIMARY KEY auto_increment not null,
-    cargo varchar(80) not null unique
-) default charset utf8;
-
+CREATE TABLE tipo_formacao (
+    id tinyint auto_increment not null PRIMARY KEY,
+    tipo varchar(30) not null unique
+)default charset utf8;
 
 CREATE TABLE formacao (
-	id tinyint PRIMARY key auto_increment not null, 
-	formacao varchar(80) not null unique
+    id int auto_increment not null PRIMARY KEY,
+    nome varchar(80) not null unique
+)default charset utf8;
+
+CREATE TABLE cursos (
+    id tinyint auto_increment not null PRIMARY KEY,
+    nome varchar(80) not null unique
+)default charset utf8;
+
+
+CREATE TABLE empresa (
+    id int auto_increment not null PRIMARY KEY,
+    nome varchar(80) not null,
+    cnpj char(14) not null unique,
+    telefone char(14) not null,
+    e_mail varchar(80) not null,
+    logradouro varchar(80) not null,
+    numero char(4) not null,
+    complemento text,
+    fk_cidade_id smallint not null
+)default charset utf8;
+ 
+ALTER TABLE empresa ADD CONSTRAINT FK_empresa_cidade
+    FOREIGN KEY (fk_cidade_id)
+    REFERENCES cidade (id)
+    on update cascade;
+    
+CREATE TABLE profissoes (
+    id tinyint auto_increment not null PRIMARY KEY,
+    cargo varchar(80) not null unique
 )default charset utf8;
 
 
 CREATE TABLE candidato (
-	id int PRIMARY KEY auto_increment not null,
+    id int auto_increment not null PRIMARY KEY,
     nome varchar(80) not null,
-    cpf char(11) unique not null,
     sexo enum('m','f') not null,
     data_de_nasc date not null,
-    estado_civil_id tinyint not null default 1,
-    telefone char(12),
-    celular char(13) not null,
+    cpf char(11) not null unique,
+    telefone char(14),
+    celular char(15) not null,
     e_mail varchar(80) not null,
-	cidade_id smallint not null,
     logradouro varchar(80) not null,
-    numero smallint not null,
+    numero char(4) not null,
     complemento text,
-    formacao_id tinyint not null,
-	cargo_id tinyint not null,
-    experiencia text not null,
-    situacao enum('A','D') not null
-) default charset utf8;
+    experiencia text,
+    situacao enum('A','D') not null,
+    fk_cidade_id smallint,
+    fk_Estado_civil_id tinyint,
+    fk_profissoes_id tinyint,
+    fk_formacao_id int,
+    fk_tipo_formacao_id tinyint
+)default charset utf8;
  
-ALTER TABLE candidato ADD CONSTRAINT cidade_candidato
-    FOREIGN KEY (cidade_id)
+ALTER TABLE candidato ADD CONSTRAINT FK_candidato_cidade
+    FOREIGN KEY (fk_cidade_id)
     REFERENCES cidade (id)
-    ON DELETE CASCADE on update cascade;
+    on update cascade;
  
-ALTER TABLE candidato ADD CONSTRAINT estado_civil_candidato
-    FOREIGN KEY (estado_civil_id)
-    REFERENCES estado_civil (id)
-    ON DELETE CASCADE on update cascade;
+ALTER TABLE candidato ADD CONSTRAINT FK_candidato_est_civ
+    FOREIGN KEY (fk_Estado_civil_id)
+    REFERENCES Estado_civil (id)
+    on update cascade;
  
-ALTER TABLE candidato ADD CONSTRAINT candidato_profissao
-    FOREIGN KEY (cargo_id)
+ALTER TABLE candidato ADD CONSTRAINT FK_candidato_prof
+    FOREIGN KEY (fk_profissoes_id)
     REFERENCES profissoes (id)
-    ON DELETE CASCADE on update cascade;
-
-Alter table candidato add constraint candidato_formacao
-foreign key (formacao_id)
-references formacao (id)
-on delete cascade on update cascade;
-    
-
-CREATE TABLE empresa (
-    id int PRIMARY KEY auto_increment not null,
-    nome varchar(80) not null,
-    cnpj char(14) unique not null,
-    telefone char(10) not null,
-    e_mail varchar(80) not null,
-	cidade_id smallint not null,
-    logradouro varchar(80) not null,
-    numero smallint not null,
-    complemento text
-) default charset utf8;
+    on update cascade;
  
-ALTER TABLE empresa ADD CONSTRAINT cidade_empresa
-    FOREIGN KEY (cidade_id)
-    REFERENCES cidade (id)
-    ON DELETE CASCADE on update cascade;
+ALTER TABLE candidato ADD CONSTRAINT FK_candidato_form
+    FOREIGN KEY (fk_formacao_id)
+    REFERENCES formacao (id)
+    on update cascade;
+ 
+ALTER TABLE candidato ADD CONSTRAINT FK_candidato_t_form
+    FOREIGN KEY (fk_tipo_formacao_id)
+    REFERENCES tipo_formacao (id)
+    on update cascade;
     
 
-CREATE TABLE vagas (
-    id int PRIMARY KEY auto_increment not null,
-    empresa_id int not null,
-	cargo_id tinyint not null,
-    formacao_id tinyint not null,
-    descrissao text not null,
-    salario decimal(5, 2),
-    quantidade_de_vagas int not null,
-    candidato_id int
-) default charset utf8;
+CREATE TABLE cursos_candidato (
+    id int auto_increment not null PRIMARY KEY,
+    duracao int not null,
+    instituicao varchar(80) not null,
+    ano year not null,
+    fk_cursos_id tinyint,
+    fk_candidato_id int
+)default charset utf8;
+ 
+ALTER TABLE cursos_candidato ADD CONSTRAINT FK_cursos
+    FOREIGN KEY (fk_cursos_id)
+    REFERENCES cursos (id)
+    on update cascade;
+ 
+ALTER TABLE cursos_candidato ADD CONSTRAINT FK_candidato
+    FOREIGN KEY (fk_candidato_id)
+    REFERENCES candidato (id)
+    on update cascade;
+    
 
-ALTER TABLE vagas ADD CONSTRAINT vagas_cargo
-    FOREIGN KEY (cargo_id)
+CREATE TABLE _vagas (
+    id int auto_increment not null PRIMARY KEY,
+    descricao text not null,
+    salario decimal(5,2),
+    quantidade_de_vagas smallint not null,
+    fk_profissoes_id tinyint not null,
+    fk_candidato_id int,
+    fk_empresa_id int not null
+)default charset utf8;
+    
+ALTER TABLE _vagas ADD CONSTRAINT FK_vagas_cargo
+    FOREIGN KEY (fk_profissoes_id)
     REFERENCES profissoes (id)
-    ON DELETE CASCADE;
+    on update cascade;
  
-ALTER TABLE vagas ADD CONSTRAINT vagas_candidato
-    FOREIGN KEY (candidato_id)
-    REFERENCES candidato (id);
+ALTER TABLE _vagas ADD CONSTRAINT FK_vagas_candidato
+    FOREIGN KEY (fk_candidato_id)
+    REFERENCES candidato (id)
+    on update cascade;
  
-ALTER TABLE vagas ADD CONSTRAINT vagas_empresa
-    FOREIGN KEY (empresa_id)
-    REFERENCES empresa (id);
+ALTER TABLE _vagas ADD CONSTRAINT FK_vagas_emp
+    FOREIGN KEY (fk_empresa_id)
+    REFERENCES empresa (id)
+    on update cascade;
     
-Alter table vagas add constraint vagas_formacao
-foreign key (formacao_id)
-references formacao (id)
-on delete cascade on update cascade;
+    
+    
+    
+    
+    
+    
+    
+    
